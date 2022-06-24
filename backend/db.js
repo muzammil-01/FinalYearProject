20500789ea643c94a8bf936b64e35ffbf67e478f
muzammil-01 <65038682+muzammil-01@users.noreply.github.com>
2022-06-25 00:01:07 +0500

    initial commit

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,4 @@
 const mongoose = require("mongoose");
-// const mongoURI = "mongodb+srv://muzzammil:hDg2jz01v0Bg5UuP@cluster0.hl0eo.mongodb.net/test"
 
 const connectToMongo = ()=>{
     mongoose.connect(`${process.env.MONGO_URI}`, ()=>{
